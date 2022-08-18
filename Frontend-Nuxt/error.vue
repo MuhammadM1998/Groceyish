93f56ce1a2c78347018a33e16859eaf4412b7a09
MuhammadM1998 <MuhammadM.1097@gmail.com>
2022-08-18 10:48:42 +0200

    Edited Error Page to add a Heading

--- Frontend-Nuxt/error.vue
+++ Frontend-Nuxt/error.vue
@@ -3,12 +3,18 @@
     <Header />
 
     <section class="app-section flex min-h-[70%]">
-      <div class="container grid place-content-center gap-12 text-center">
+      <div class="container grid place-content-center gap-4 text-center">
         <img
           src="~/assets/icons/Error.svg"
           alt="Error Illustration"
           class="lg:h-[350px] lg:w-[500px]"
         />
+
+        <h2>Oops! Something went wrong</h2>
+
+        <p class="font-medium text-gray-200">
+          The page you requested can't be reached right now.
+        </p>
 
         <NuxtLink to="/" class="app-button mx-auto w-fit bg-blue-200">
           Go Back Home
